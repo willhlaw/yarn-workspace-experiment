b1ebacf1e36de42312ccef5dc33ccdcc006ac528
willhlaw <will.lawrence@gmail.com>
2017-12-26 17:34:34 -0500

    Init yarn workspace example

---
+++ workspace-a/index.js
@@ -0,0 +1,3 @@
+exports.workspaceA = {
+  hi: 'hello'
+};
---
+++ workspace-b/index.js
@@ -0,0 +1,5 @@
+var workspaceA = require('workspace-a');
+var chalk = require('chalk');
+
+console.log('workspaceA', workspaceA);
+console.log('chalk', chalk);
